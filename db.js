b6e5022cff0e79747cb8bac47f934fcf59e2b5ed
wassss95 <wassila.elfarh@edu.ece.fr>
2023-11-23 11:07:40 +0100

    maj

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
